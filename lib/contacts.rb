a6b799130d491c4214272484c1530b73142efa43
Damian Malysza <damian.malysza@gmail.com>
2020-10-23 00:25:06 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |k,v|
     data.each do |k,v|
       binding.pry
-      if k == :favorite_ice_cream_flavors
+      #if k == :favorite_ice_cream_flavors
 
     end
   end
